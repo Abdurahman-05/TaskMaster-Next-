8a51f7985254a05bc74c2c1923c3b7b6bc5cb556
Abdurahman <eabdurahman8@gmail.com>
2025-04-06 10:32:54 +0300

    changing icon color

--- client/src/app/homepage/page.tsx
+++ client/src/app/homepage/page.tsx
@@ -85,7 +85,7 @@ export default function home() {
                 <button className="p-2">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-6 text-gray-400"
+                    className="h-6 w-6 text-[#494C6B]"
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
