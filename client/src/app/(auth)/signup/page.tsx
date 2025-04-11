1b0bb9fd37c9f829924ff47c031a3f44ae14a6b0
Abdurahman <eabdurahman8@gmail.com>
2025-04-11 11:09:59 +0300

    mobile version of signup page '

--- client/src/app/(auth)/signup/page.tsx
+++ client/src/app/(auth)/signup/page.tsx
@@ -1,11 +1,63 @@
-import React from 'react'
-
+import React from "react";
+import { FaArrowRight } from "react-icons/fa";
 export default function Signup() {
   return (
-    <div style={{ backgroundImage: "url('/images/h_page/bg_img.svg')" }} className='h-screen w-full bg-no-repeat bg-cover bg-center flex justify-center items-cente' >
-      <div className="h-7 w-6 bg-no-repeat bg-cover bg-center flex justify-center items-center " style={{ backgroundImage: "url('/images/h_page/bg_img.svg')" }}>
+    <div
+      style={{ backgroundImage: "url('/images/h_page/bg_img.svg')" }}
+      className="h-fit w-full bg-no-repeat bg-cover bg-center  px-4 pt-7 pb-40"
+    >
+      <div
+        className="w-full bg-no-repeat bg-cover bg-top h-[280px] p-3 drop-shadow-lg"
+        style={{ backgroundImage: "url('/images/aut/insidecover.png')" }}
+      >
+        <div className="w-full h-full z-[1000] bg-[rgba(255,255,255,0.3)]  backdrop-blur-sm flex flex-col justify-center items-center gap-5">
+          <img src="/images/aut/logo.svg" alt="" />
+          <h1 className=" text-4xl text-white font-josefin font-medium">
+            Your Notes
+          </h1>
+        </div>
       </div>
-      <div className="h-7 w-6 bg-green-400"></div>
+
+      <div className="w-full bg-white h-fit drop-shadow-2xl pb-7">
+        <div className="pt-[45px] text-center font-bold text-4xl text-primary">
+          Signup
+        </div>
+        <form action="" className="flex flex-col px-3 space-y-5 ">
+          <div className="flex flex-col text-secondery font-bold gap-3 text-2xl">
+            <p className="text-2xl">Email</p>
+            <input
+              type="email"
+              className="h-[50px] w-full bg-white  border-[0.5px] border-[rgba(73,76,107,0.2)] rounded-lg outline-none pl-2 text-secondery font-medium text-xl active:bg-white drop-shadow-md"
+            />
+          </div>
+          <div className="flex flex-col text-secondery font-bold gap-3 text-2xl">
+            <p className="text-2xl">Password</p>
+            <input
+              type="password"
+              className="h-[50px] w-full bg-white  border-[0.5px] border-[rgba(73,76,107,0.2)] rounded-lg outline-none pl-2 text-secondery font-medium text-xl active:bg-white"
+            />
+          </div>
+          <div className="flex flex-col text-secondery font-bold gap-3 pb-7 ">
+            <p className="text-2xl">Confirm password</p>
+            <input
+              type="password"
+              className="h-[50px] w-full bg-white  border-[0.5px] border-[rgba(73,76,107,0.2)] rounded-lg outline-none pl-2 text-secondery font-medium text-xl active:bg-white"
+            />
+          </div>
+
+          <button className="w-full bg-primary text-white font-bold text-2xl py-3 rounded-lg flex justify-center gap-3 items-center drop-shadow-md">
+            Complete Signup <FaArrowRight />
+          </button> 
+          <div className="space-y-3 flex flex-col justify-center pt-5">
+            <p className="text-gray-500 text-2xl font-josefin text-center">
+              Alrady have an account!
+            </p>
+            <button className="font-bold text-2xl text-primary pb-7">
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
--- client/src/app/homepage/layout.tsx
+++ client/src/app/homepage/layout.tsx
@@ -31,42 +31,50 @@ export default function NavLayout({ children }: { children: React.ReactNode }) {
 
   return (
     <div className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-     
-        <nav className="h-[62px] w-full flex items-center relative">
-          <div className="ml-5">
-            <img src="/images/h_page/logo-pink.svg" alt="logo" />
-          </div>
-          <h1 className="mr-auto ml-2 text-2xl text-primary">Your Notes</h1>
-          <div className="flex space-x-4 mr-6">
-            <button
-              onClick={handleLang}
-              className="font-poppins text-3xl text-primary font-bold cursor-pointer"
-            >
-              {lang}
-            </button>
-            <button onClick={handleTheme}>
-              <img
-                src={
-                  theme === "Dark" ? "/images/h_page/light.svg" : "/images/h_page/dark.svg"
-                }
-                alt="dark mode toggle"
-              />
-            </button>
-            <button onClick={handleUser}>
-              <img src="/images/h_page/user.svg" alt="user profile" />
-            </button>
-          </div>
+      <nav className="h-[62px] w-full flex items-center relative">
+        <div className="ml-5">
+          <img src="/images/h_page/logo-pink.svg" alt="logo" />
+        </div>
+        <h1 className="mr-auto ml-2 text-2xl text-primary">Your Notes</h1>
+        <div className="flex space-x-4 mr-6">
+          <button
+            onClick={handleLang}
+            className="font-poppins text-3xl text-primary font-bold cursor-pointer"
+          >
+            {lang}
+          </button>
+          <button onClick={handleTheme}>
+            <img
+              src={
+                theme === "Dark"
+                  ? "/images/h_page/light.svg"
+                  : "/images/h_page/dark.svg"
+              }
+              alt="dark mode toggle"
+            />
+          </button>
+          <button onClick={handleUser}>
+            <img src="/images/h_page/user.svg" alt="user profile" />
+          </button>
+        </div>
         {isUserOpen && (
-          <div className=" w-full min-h-screen bg-[rgba(0,0,0,0.8)]   absolute top-[63px] space-y-3   flex flex-col justify-center items-center z-[1000] 
+          <div
+            className=" w-full min-h-screen bg-[rgba(0,0,0,0.8)]   absolute top-[63px] space-y-3   flex flex-col justify-center items-center z-[1000] 
           sm:w-[300px] sm:h-fit sm:rounded-2xl sm:bg-white sm:top-[65px] sm:right-[20px] sm:px-3 sm:py-3 sm:space-y-1 sm:min-h-0
-          ">
-            <p className=" font-bold text-primary text-2xl pb-5 text-left sm:pl-2 sm:pb-8  sm:mb-auto">hi mohammed</p>
-            <button className="bg-[#7C8495] text-2xl py-4 px-6 rounded-md w-[321px] font-medium sm:py-3 sm:px-2 sm:w-full  text-white">Modify user info</button>
-            <button className="bg-primary  text-white text-2xl py-4 px-6 rounded-md w-[321px] font-medium sm:w-full sm:py-3 sm:px-2 ">Logout</button>
+          "
+          >
+            <p className=" font-bold text-primary text-2xl pb-5 text-left sm:pl-2 sm:pb-8  sm:mb-auto">
+              hi mohammed
+            </p>
+            <button className="bg-[#7C8495] text-2xl py-4 px-6 rounded-md w-[321px] font-medium sm:py-3 sm:px-2 sm:w-full  text-white">
+              Modify user info
+            </button>
+            <button className="bg-primary  text-white text-2xl py-4 px-6 rounded-md w-[321px] font-medium sm:w-full sm:py-3 sm:px-2 ">
+              Logout
+            </button>
           </div>
         )}
-       
-        </nav>
+      </nav>
 
       {/* sm:w-[200px] sm:h-fit sm:rounded-2xl sm:bg-white sm:top-[65px] sm:right-[20px] sm:px-3 sm:py-3 sm:space-y-1 */}
 
